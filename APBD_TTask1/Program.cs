f587bc7e57d9df3ece30a2f464f752e74e82d239
s33440-cpu <s33440@pjwstk.edu.pl>
2026-03-17 22:09:24 +0100

    New method on f-a

--- APBD_TTask1/Program.cs
+++ APBD_TTask1/Program.cs
@@ -1,3 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a number:");
+string input = Console.ReadLine();
 
-Console.WriteLine("Hey");
+if (int.TryParse(input, out int number))
+{
+    Console.WriteLine(number);
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid number.");
+}
+
+
+List<int> numbers = new List<int> { number, 5, 10, 15 };
+double average = StatisticsHelper.CalculateAverage(numbers);
+Console.WriteLine($"Average: {average}");
+    
+
+public static class StatisticsHelper{
+        public static double CalculateAverage(List<int> numbers)
+        {
+            return numbers.Average();
+        }
+
+        public static double CalculateAverage(int[] values)
+        {
+            if (values.Length == 0) return 0;
+            return values.Average();
+        }
+}
